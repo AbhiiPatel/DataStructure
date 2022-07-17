4b37a049ffeeeca3a12968665e6c005af4d3d9ff
AbhiiPatel <avpatel1904@gmail.com>
2022-07-17 14:58:54 +0530

    tree traversal

--- treeBst1.c
+++ treeBst1.c
@@ -38,21 +38,64 @@ struct node* addNode(int num,struct node *root){
 	return root;
 }
 
+void inOrder(struct node *root){
+	if(root==NULL)
+		return;
+	inOrder(root->left);
+	printf("%d ",root->data);
+	inOrder(root->right);
+	
+}
 
+void preOrder(struct node *root){
+	if(root==NULL)
+		return;
+	printf("%d ",root->data);
+	preOrder(root->left);
+	preOrder(root->right);
+	
+}
+
+void postOrder(struct node *root){
+	if(root==NULL)
+		return;
+	postOrder(root->left);
+	postOrder(root->right);
+	printf("%d ",root->data);
+}
 int main(){
 	
+	/*
 	root=addNode(50,root);
 		 addNode(35,root);
 		 addNode(100,root);
 		 addNode(25,root);
 		 addNode(150,root);
 	
-	/*
+	
 	printf("%d",root->data);  //50
 	printf("\n%d",root->left->data);  //35
 	printf("\n%d",root->right->data); //100
 	printf("\n%d",root->left->left->data);  //25
 	printf("\n%d",root->right->right->data);  //150
 	*/
+	
+	
+	root=addNode(50,root);
+		addNode(35,root);
+		addNode(25,root);
+		addNode(40,root);
+		addNode(100,root);
+		addNode(80,root);
+		addNode(150,root);
+		
+		
+	printf("\nIn order Traversal:\n");
+	inOrder(root);
+	printf("\nPre order Traversal:\n");
+	preOrder(root);
+	printf("\nPost order Traversal:\n");
+	postOrder(root);
+	
 	return 0;
 }
